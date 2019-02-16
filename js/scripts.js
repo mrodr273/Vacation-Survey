0b3f80393766b6f806ba82b3179964ce1850bed9
Connie <connie.rdz.1995@gmail.com>
2019-02-16 15:51:18 -0800

    create a basic website layout.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+/*$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});*/
+
+/*$(".my-class").show();
+$(".my-class").hide();
+$(".my-class").toggle();*/
+
+/*Add a CSS class to an element:
+$("body").addClass("my-class");*/
+
+/*var add = function(number1, number2) {
+  return number1 + number2;
+};*/
+
+/*var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));*/
+
+/*$('.element-to-select').click(function() {
+  alert('hi');
+});*/
+
+/*Test that you've selected the correct DOM element:
+$('.element-to-select').css('background-color', 'green');*/
+
+/*.clickable {
+  cursor: pointer;
+  color: #0088cc;
+.clickable:hover {
+  text-decoration: underline;
+}*/
