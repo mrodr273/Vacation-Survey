c20e9c033af20b382026e3840c2907169973f680
Connie <connie.rdz.1995@gmail.com>
2019-02-17 15:13:16 -0800

    adding a readme.md.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Vacation-Website.html
+
+#### This website offers vacation suggestions after the user answer a survey, February 17th, 2019.
+
+#### By Connie Rodriguez
+
+## Description
+
+The user picks one type of vacation from three choices: tropical paradises, outdoor adventures and road trips. Then the user is taken to a 5-questions survey. The websites suggests a vacation idea depending on the answers.
+
+## Setup/Installation Requirements
+
+* Double click on Vacation-Website.html to open it.
+* Pick a vacation type
+* Answer the survey questions
+* Click Submit
+
+## Known Bugs
+
+Not known bugs.
+
+## Support and contact details
+
+Contact Connie Rodriguez at connie.rdz.1995@gmail.com
+
+## Technologies Used
+
+HTML
+Bootstrap
+Jquery
+
+### License
+
+Copyright (c) 2019 **Connie Rodriguez**
--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,6 @@ $(document).ready(function() {
     });
   });
 
-  /*$("form").submit(function(event) {
-    event.preventDefault();
-    var Q1value = parseInt($("input:radio[name=Q1]:checked").val(););
-    var Q2value = parseInt($("input:radio[name=Q2]:checked").val(););
-    var Q3value = parseInt($("input:radio[name=Q3]:checked").val(););
-    var Q4value = parseInt($("input:radio[name=Q4]:checked").val(););
-    var Q5value = parseInt($("input:radio[name=Q5]:checked").val(););
-    var result= Q1value + Q2value + Q3value + Q4value + Q5value;
-    alert(result)
-    $("#output").text(result);
-  });*/
-
   $('img#outdoors').click(function() {
     alert("Ready for a new adventure?")
     $(".outdoors").show();
@@ -38,44 +26,6 @@ $(document).ready(function() {
     alert("Let's do it!")
     $(".roadtrip").show();
   });
-
-  /*$("form").submit(function(event) {
-    event.preventDefault();
-    var Q1value = parseInt($("input:radio[name=Q1]:checked").val(););
-    var Q2value = parseInt($("input:radio[name=Q2]:checked").val(););
-    var Q3value = parseInt($("input:radio[name=Q3]:checked").val(););
-    var Q4value = parseInt($("input:radio[name=Q4]:checked").val(););
-    var Q5value = parseInt($("input:radio[name=Q5]:checked").val(););
-    var result= Q1value + Q2value + Q3value + Q4value + Q5value;
-    alert(result)
-    $("#output").text(result);
-  });*/
-
 });
-
-
-
-/*$(".my-class").show();
-$(".my-class").hide();
-$(".my-class").toggle();*/
-
-/*Add a CSS class to an element:
-$("body").addClass("my-class");*/
-
-
 /*var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));*/
-
-
-
-/*Test that you've selected the correct DOM element:
-$('.element-to-select').css('background-color', 'green');*/
-
-/*.clickable {
-  cursor: pointer;
-  color: #0088cc;
-.clickable:hover {
-  text-decoration: underline;
-}*/
+var number2 = parseInt(prompt("Enter another number:"));*/
